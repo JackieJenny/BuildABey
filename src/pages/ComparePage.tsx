216634ef79be0810366a9cfc9e1e09d2ea3c0367
Cole <cmon56232@gmail.com>
2025-08-02 17:31:42 +1200

    dual comparisons

--- src/pages/ComparePage.tsx
+++ src/pages/ComparePage.tsx
@@ -3,26 +3,38 @@ import { OrbitControls } from '@react-three/drei'
 import TestBeyModel from '../components/TestBeyModel'
 
 
-const Scene = () => {
+const SceneCanvas = ({ children }: { children: React.ReactNode }) => (
+  <div style={{ flex: 1, height: '100vh' }}>
+    <Canvas camera={{ position: [0, 2, 5], fov: 60 }}>
+      <ambientLight intensity={0.8} />
+      <directionalLight
+        intensity={1}
+        position={[0, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <OrbitControls />
+      {children}
+    </Canvas>
+  </div>
+)
+
+const ComparePage = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas camera={{ position: [0, 2, 5], fov: 60 }}>
-        <ambientLight intensity={0.8} />
-        <directionalLight
-          intensity={1}
-          position={[0, 5, 5]}
-          castShadow
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-        />
-        <OrbitControls />
-        <mesh position={[0, 0, 0]}>
-          <TestBeyModel />
+    <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+      <SceneCanvas>
+        <TestBeyModel key="left" />
+      </SceneCanvas>
+      <SceneCanvas>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
         </mesh>
-      </Canvas>
+      </SceneCanvas>
     </div>
   )
 }
 
 
-export default Scene
+export default ComparePage
