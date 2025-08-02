8c930fd1d662d7b99277179d14cb7863449e392a
Cole <cmon56232@gmail.com>
2025-08-02 17:12:11 +1200

    beyblade rendering

--- src/pages/ComparePage.tsx
+++ src/pages/ComparePage.tsx
@@ -2,17 +2,22 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import TestBeyModel from '../components/TestBeyModel'
 
+
 const Scene = () => {
   return (
-    <>
-      <Canvas camera={{ position: [0, 0, 5] }}>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [0, 2, 5], fov: 60 }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[0, 5, 5]} />
-        <TestBeyModel />
+        <directionalLight position={[5, 5, 5]} />
         <OrbitControls />
+        <mesh position={[0, 0, 0]}>
+          <TestBeyModel />
+          <meshStandardMaterial color="orange" />
+        </mesh>
       </Canvas>
-    </>
+    </div>
   )
 }
+
 
 export default Scene
