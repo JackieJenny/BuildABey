600b6176781d8b9747beee549c74b98191611320
Cole <cmon56232@gmail.com>
2025-08-02 17:20:24 +1200

    lighting changes

--- src/pages/ComparePage.tsx
+++ src/pages/ComparePage.tsx
@@ -7,12 +7,17 @@ const Scene = () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 2, 5], fov: 60 }}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} />
+        <ambientLight intensity={0.8} />
+        <directionalLight
+          intensity={1}
+          position={[0, 5, 5]}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
         <OrbitControls />
         <mesh position={[0, 0, 0]}>
           <TestBeyModel />
-          <meshStandardMaterial color="orange" />
         </mesh>
       </Canvas>
     </div>
