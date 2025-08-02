417380a8ee41023595a0a04aa8967c27c27814d7
Liphaa <165632031+Liphaa@users.noreply.github.com>
2025-08-03 00:17:57 +1200

    frosted glass div

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,18 +5,20 @@ export default function LandingPage() {
 
   return (
     <>
-        <div 
+      <div 
         className="h-dvh w-dvw flex flex-col justify-center items-center bg-gray-900 text-white pt-16 bg-[url('/images/Background.png')] 
-        bg-cover bg-center">
-
+        bg-cover bg-center"
+      >
+        <div className="backdrop-blur-md bg-glassgrey/30 w-3/4 h-3/4 rounded-xl p-8 border border-gray-500/50 shadow-lg shadow-inner">
           <h1 className="text-5xl font-bold mb-8">BUILD A BEY</h1>
           <button
-              onClick={() => navigate('/create')}
-              className="bg-blue-600 px-6 py-3 rounded-xl text-xl hover:bg-blue-700 transition"
+            onClick={() => navigate('/create')}
+            className="bg-blue-600 px-6 py-3 rounded-xl text-xl hover:bg-blue-700 transition"
           >
-              Create Beyblade, random push
+            Create Beyblade, random push
           </button>
         </div>
+      </div>
     </>
   )
 }
