58dbf3d0aec47a6dfe103c31b56bebcbf6ac8f61
Cole <cmon56232@gmail.com>
2025-08-02 13:17:12 +1200

    Main page setup

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+
+export default function LandingPage() {
+  const navigate = useNavigate()
+  return (
+    <>
+        <div className="h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
+        <h1 className="text-5xl font-bold mb-8">Create Your Beyblade</h1>
+        <button
+            onClick={() => navigate('/create')}
+            className="bg-blue-600 px-6 py-3 rounded-xl text-xl hover:bg-blue-700 transition"
+        >
+            Create Beyblade
+        </button>
+        </div>
+    </>
+  )
+}
