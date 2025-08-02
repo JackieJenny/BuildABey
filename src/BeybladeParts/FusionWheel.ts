47b1b34a407f89e74fb58dd3d1a55a88735f9de2
Cole <cmon56232@gmail.com>
2025-08-02 15:48:29 +1200

    created compare pages

--- src/BeybladeParts/FusionWheel.ts
+++ src/BeybladeParts/FusionWheel.ts
@@ -3,3 +3,7 @@ export const FusionWheel = [
     {id: 'LdragoWheel', name: "LdragoWheel", attack: 6, defense: 2, stamina: 5},
     {id: 'LeoneWheel', name: "LeoneWheel", attack: 3, defense: 7, stamina: 5}
 ];
+
+// Bolt, spintrack, tip
+
+
---
+++ src/pages/ComparePage.tsx
@@ -0,0 +1,9 @@
+export default function ComparePage() {
+  return (
+    <>
+        <div>
+        <h1>Compare to Meta Builds</h1>
+        </div>
+    </>
+  )
+}
