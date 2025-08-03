0cf068648a30fdd13306bea2a07788957323f33c
boston8 <bstnthmsn@gmail.com>
2025-08-03 12:39:51 +1200

    add custom bey to collection

--- src/components/BeyCollectionData.tsx
+++ src/components/BeyCollectionData.tsx
@@ -1,6 +1,7 @@
 export const beybladeCollection = [
-  { id: 1, image: '/images/completeRenders/pegasusfull.png', name: 'Pegasus' },
-  { id: 2, image: '/images/completeRenders/leonefull.png', name: 'Leone Storm' },
-  { id: 3, image: '/images/completeRenders/ldragofull.png', name: 'L-Drago Destroy' },
-  { id: 4, image: '/images/completeRenders/capricornefull.png', name: 'Capricorn',  },
+  { id: 1, image: '/images/completeRenders/customfull.png', name: 'Custom Bey 1' },
+  { id: 2, image: '/images/completeRenders/pegasusfull.png', name: 'Pegasus' },
+  { id: 3, image: '/images/completeRenders/leonefull.png', name: 'Leone Storm' },
+  { id: 4, image: '/images/completeRenders/ldragofull.png', name: 'L-Drago Destroy' },
+  { id: 5, image: '/images/completeRenders/capricornefull.png', name: 'Capricorn',  },
 ];
